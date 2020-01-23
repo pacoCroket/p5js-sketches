83364ade314652adb302c0515a98f8e80d961f5c
pacoCroket pi <croketproduction@gmail.com>
2020-01-23 12:26:18 +0000

    added bash autoLoad.sh

---
+++ finding-people/autoLoad.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+# fetch changes from repo
+git pull
+
+# Run this script in display 0 - the monitor
+export DISPLAY=:0
+ 
+# Hide the mouse from the display
+unclutter &
+ 
+# If Chrome crashes (usually due to rebooting), clear the crash flag so we don't have the annoying warning bar
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+ 
+# Run Chromium and open tabs
+/usr/bin/chromium-browser --window-size=480,320 --kiosk --window-position=0,0 index.html &
