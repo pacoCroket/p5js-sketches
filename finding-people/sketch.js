bb32aeacbc0b1209f3a794d7b82304e2f670d01a
Paco Croket <croketproduction@gmail.com>
2020-01-23 15:47:59 +0100

    trying to get multiple poses to work

--- finding-people/sketch.js
+++ finding-people/sketch.js
@@ -1,20 +1,21 @@
+let cnv;
 let video;
 let poseNet;
-let noseX = 0;
-let noseY = 0;
-let eyelX = 0;
-let eyelY = 0;
+let faces = [];
+let poses = [];
 let pose;
+let leftEye;
+let rightEye;
 
 var myAsciiArt;
-var asciiart_width = 240; var asciiart_height = 120;
+var asciiart_width = 80; var asciiart_height = 60;
 var asciiart_scale = 4;
 /*
 Buffer for processed graphics, simplifying some operations. This will be an
 object derived from the p5.Graphics class
 */
 var gfx;
-var showOryginalImageFlag = true;
+var showOryginalImageFlag = false;
 let maxFontSize;
 let xRescale;
 let yreScale;
@@ -23,16 +24,18 @@ let mic;
 var asciiartOn = true;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    cnv = createCanvas(1280, 960);
+    cnv.parent("#sketch");
+    cnv.class("center");
+
     video = createCapture(VIDEO);
-    video.size(640, 360);
+    video.size(640, 480);
     video.elt.setAttribute('playsinline', '');
     video.hide();
     
     // ASCII art
     myAsciiArt = new AsciiArt(this);
     textAlign(CENTER, CENTER); textFont('monospace', 8); textStyle(NORMAL);
-    noStroke(); fill(255);
     gfx = createGraphics(asciiart_width, asciiart_height);
     gfx.pixelDensity(1);
     maxFontSize = width/asciiart_width*1.5;
@@ -46,20 +49,27 @@ function setup() {
     // ML5
     poseNet = ml5.poseNet(video, modelReady);
     poseNet.on('pose', gotPoses);
+    strokeCap(ROUND);
 }
 
-function gotPoses(poses) {
-    // console.log(poses);
-    if (poses.length > 0) {
-        pose = poses[0];
-        let nX = poses[0].pose.keypoints[0].position.x;
-        let nY = poses[0].pose.keypoints[0].position.y;
-        let eX = poses[0].pose.keypoints[1].position.x;
-        let eY = poses[0].pose.keypoints[1].position.y;
-        noseX = lerp(noseX, nX, 0.5);
-        noseY = lerp(noseY, nY, 0.5);
-        eyelX = lerp(eyelX, eX, 0.5);
-        eyelY = lerp(eyelY, eY, 0.5);
+function gotPoses(_poses) {
+    poses = _poses;
+    if (_poses.length > 0) {
+        for (var i = 0; i < _poses.length; i++) {  
+            let nX = _poses[i].pose.keypoints[0].position.x;
+            let nY = _poses[i].pose.keypoints[0].position.y;
+            let elX = _poses[i].pose.keypoints[1].position.x;
+            let elY = _poses[i].pose.keypoints[1].position.y;
+            let erX = _poses[i].pose.keypoints[2].position.x;
+            let erY = _poses[i].pose.keypoints[2].position.y;
+
+            if (faces[i] != undefined && faces[i].length == 6) {
+                faces[i] = [lerp(faces[i][0], nX, 0.5), lerp(faces[i][1], nY, 0.5), lerp(faces[i][2], elX, 0.5), lerp(faces[i][3], elY, 0.5),
+                lerp(faces[i][4], erX, 0.5), lerp(faces[i][5], erY, 0.5)];
+            } else {
+                faces[i] = [nX, nY, elX, elY, erX, erY];
+            }
+        }
     }
 }
 
@@ -73,24 +83,10 @@ function draw() {
     translate(width, 0);
     scale(-1.0, 1.0);    // flip x-axis backwards
 
-    if (showOryginalImageFlag) image(video, 0, 0);
+    if (showOryginalImageFlag) image(video, 0, 0, width, height);
 
-    // draw ML5
-
-    let d = dist(noseX, noseY, eyelX, eyelY);
-
-    noFill();
-    strokeWeight(5);
-    stroke(0, 255, 120);
-    ellipse(noseX*xRescale/2, noseY*yRescale/2, d*5);
-    if (pose != undefined) {
-        for (var i = 0; i < pose.pose.keypoints.length; i++) {
-            ellipse(pose.pose.keypoints[i].position.x, pose.pose.keypoints[i].position.y, 20);
-        }
-        for (var i = 0; i < pose.skeleton.length; i++) {
-            line(pose.skeleton[i][0].position.x, pose.skeleton[i][0].position.y, pose.skeleton[i][1].position.x, pose.skeleton[i][1].position.y);
-        }
-    }
+    noStroke();
+    fill(255);   
     
     if (asciiartOn) {
         // draw some ASCII art
@@ -102,16 +98,73 @@ function draw() {
         different values may have the best effect. And sometimes it is worth not
         to apply the effect of posterization on the image.
         */
-        gfx.filter(POSTERIZE, map(mouseY, 0, height, 3, 60));
+        // let posterize = map(mouseY, 50, height, 2, 60);
+        // posterize = constrain(posterize, 2, 60);
+        // gfx.filter(POSTERIZE, posterize);
+        gfx.filter(POSTERIZE, 3);
+        gfx.filter(INVERT);
         // textFont('monospace', map(mic.getLevel(), 0, 1, 4, 20));
         
-        textFont('monospace', map(mouseX, 0, width, 2, maxFontSize));
+        // textFont('monospace', map(mouseX, 0, width, 2, maxFontSize));
+        textFont('monospace', 10);
         ascii_arr = myAsciiArt.convert(gfx);
         myAsciiArt.typeArray2d(ascii_arr, this);
     }
 
-    copy(video.width, 0, video.width, video.height, width, 0, width, height);
-    ellipse(width/2, height/2, 50, 50);
+    // circle over head reacting to mic
+    stroke(255);
+    strokeWeight(6);
+
+    // skeleton and eyes
+    if (poses != undefined) {
+        for (var i = 0; i < poses.length; i++) {
+            let pose = poses[i];
+            
+            // for (var i = 0; i < pose.pose.keypoints.length; i++) {
+            //     ellipse(pose.pose.keypoints[i].position.x*xRescale, pose.pose.keypoints[i].position.y*yRescale, 20);
+            // }
+            for (var i = 0; i < pose.skeleton.length; i++) {
+                line(pose.skeleton[i][0].position.x*xRescale, pose.skeleton[i][0].position.y*yRescale, pose.skeleton[i][1].position.x*xRescale, pose.skeleton[i][1].position.y*yRescale);
+            }
+        }
+
+        for (var i = 0; i < faces.length; i++) {
+            let noseX = faces[i][0];
+            let noseY = faces[i][1];
+            let elX = faces[i][2];
+            let elY = faces[i][3];
+            let erX = faces[i][4];
+            let erY = faces[i][5];
+            let d = dist(noseX, noseY, elX, elY);
+
+            // circle ovr head
+            fill(0, 130);
+            push();
+            // let btwEyes = p5.Vector.sub()
+            ellipse(noseX*xRescale, noseY*yRescale-d, d*8*map(mic.getLevel(), 0, 1, 1, 0.6), d*10*map(mic.getLevel(), 0, 1, 1, 1.5));
+            push();
+
+            // black crosses on the eyes
+            stroke(0);
+            strokeWeight(d*0.6);
+            // left eye
+            push();
+            translate(elX*xRescale, elY*yRescale);
+            rotate(PI/4);
+            line(- d*0.7, 0, d*0.7, 0);
+            rotate(PI/2);
+            line(- d*0.7, 0, d*0.7, 0);
+            pop();
+            // right eye
+            push();
+            translate(erX*xRescale, erY*yRescale);
+            rotate(PI/4);
+            line(- d*0.7, 0, d*0.7, 0);
+            rotate(PI/2);
+            line(- d*0.7, 0, d*0.7, 0);
+            pop();
+        }
+    }
 
 
 }
