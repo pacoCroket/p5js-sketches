a6081db1055a145254a65266c337b6e6f1a7d12a
Paco Croket <croketproduction@gmail.com>
2020-02-11 00:43:13 +0100

    change autoLoad to go to gitpages

--- finding-people/autoLoad.sh
+++ finding-people/autoLoad.sh
@@ -14,4 +14,4 @@ sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromi
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
  
 # Run Chromium and open tabs
-/usr/bin/chromium-browser  --noerrdialogs --kiosk --start-fullscreen index.html &
+/usr/bin/chromium-browser  --noerrdialogs --kiosk --start-fullscreen https://pacocroket.github.io/p5js-sketches/finding-people/ &
